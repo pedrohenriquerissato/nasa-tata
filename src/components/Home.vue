8cf7b61b73aa8de60b49e0508793b4803e180911
Pedro Henrique Rissato <pedrorissato@usp.br>
2019-10-21 21:32:10 -0300

    Final version

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,10 +5,10 @@
 
 
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Abrir Navegação">
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Abrir Navegação" style="padding: 0.05rem .75rem;">
         <span class="navbar-toggler-icon"></span>
       </button>
-
+<!-- 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto">
 
@@ -16,7 +16,7 @@
             <a class="btn btn-outline-primary" href="inscricao.html">Register</a>
           </li>
         </ul>
-      </div>
+      </div> -->
     </nav>
 
 
@@ -86,6 +86,11 @@
                 </div>
               </div>
 
+              <div class="form-inline">
+                <label for=""><b>Please take a picture:</b></label>
+                  <!-- <img src="../assets/camera.svg" alt="" height="50" width="75" style="border-radius: 25px"> -->
+                <svg id="Layer_1" style="enable-background:new 0 0 24 24; height: 55px; width: 65px; margin-left: 10px" version="1.1" viewBox="0 0 24 24" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M19.599,6h-2.323l-0.714-2.015C16.34,3.319,15.719,3,15.017,3H8.983C8.281,3,7.66,3.319,7.438,3.985L6.724,6H4.401  C2.905,6,2,7.247,2,8.743v9.77C2,20.009,2.905,21,4.401,21h15.197C21.095,21,22,20.009,22,18.513v-9.77C22,7.247,21.095,6,19.599,6z   M21,18.513C21,19.411,20.497,20,19.599,20H4.401C3.503,20,3,19.411,3,18.513v-9.77C3,7.846,3.503,7,4.401,7h2.714  C7.349,7,7.556,6.908,7.63,6.687l0.838-2.415C8.542,4.05,8.749,4,8.983,4h6.034c0.234,0,0.441,0.078,0.515,0.301l0.838,2.421  C16.444,6.943,16.651,7,16.885,7h2.714C20.497,7,21,7.846,21,8.743V18.513z"/><path d="M12,7.658c-2.993,0-5.428,2.435-5.428,5.428S9.007,18.513,12,18.513s5.428-2.435,5.428-5.428S14.993,7.658,12,7.658z   M12,17.428c-2.394,0-4.342-1.948-4.342-4.342S9.606,8.743,12,8.743s4.342,1.948,4.342,4.342S14.394,17.428,12,17.428z"/></svg>
+              </div>
               <!-- <div class="imagePreview"></div> -->
 
               <button class="btn btn-danger btn-rounded btn-block z-depth-0 my-4 waves-effect"
@@ -350,7 +355,7 @@
   }
 
   .navbar {
-    height: 64px;
+    height: 60px;
     box-shadow: 0 2px 4px 0 rgba(0, 0, 0, .05), 0 4px 8px 0 rgba(0, 0, 0, .05);
   }
 
@@ -364,6 +369,12 @@
     background-repeat: no-repeat;
     display: inline-block;
     box-shadow: 0px -3px 6px 2px rgba(0, 0, 0, 0.2);
+  }
+
+  .bola {
+    position: relative;
+    border-radius: 10px;
+    border: solid 1px #cccc;
   }
 
   /* Somente botao upload da foto */
